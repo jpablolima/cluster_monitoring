8b05cfcbd78ba4904d4f341a3e90cad401429c8e
João Pablo <jp.pablolima@gmail.com>
2025-06-06 07:08:53 -0300

    listando as images dos pods

--- kubernetes-get-images.py
+++ kubernetes-get-images.py
@@ -8,7 +8,16 @@ v1 = client.CoreV1Api()
 def get_pods_for_all_namespaces() -> list:
     return v1.list_pod_for_all_namespaces().items
 
+
+def get_images_from_pod(pod) -> list:
+    images = []
+    for container in pod.spec.containers:
+        images.append(container.image)
+    return images 
+
+def main():
+    for pod in get_pods_for_all_namespaces():
+        print(f"Container name: {pod.metadata.name} | images: {get_images_from_pod(pod)}")
+
 if __name__ == "__main__":
-    pods = get_pods_for_all_namespaces()
-    for pod in pods:
-        print(pod.metadata.name)
+    main()
